203f65282ccdfee12a7cd0a9c7583d35c75e7646
Sumanth Kumar Mora <sumanthkumar537@gmail.com>
2018-06-28 14:59:08 +0530

    Extract severe log messages sorted

--- src/Spring13/Week2/LogAnalysis.hs
+++ src/Spring13/Week2/LogAnalysis.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS_GHC -Wall #-}
 module Spring13.Week2.LogAnalysis
-  ( parse
+  ( parse, whatWentWrong
   ) where
 
 import Spring13.Week2.Log
@@ -37,13 +37,40 @@ parseError unknownTypeMessage = Unknown (intercalate " " unknownTypeMessage)
 getWords :: String -> [String]
 getWords line = words line
 
-generateTree :: [String] -> MessageTree
-generateTree messages = foldl (\tree message -> Spring13.Week2.LogAnalysis.insert (parseMessage message) tree) Leaf messages
-
 insert :: LogMessage -> MessageTree -> MessageTree
 insert (Unknown _) messageTree = messageTree
 insert newLog Leaf = Node Leaf newLog Leaf
 insert newLog (Node leftTree currentLog rightTree) = if isLessThan newLog currentLog then Node (Spring13.Week2.LogAnalysis.insert newLog leftTree) (currentLog) (rightTree) else Node (leftTree) (currentLog) (Spring13.Week2.LogAnalysis.insert newLog rightTree)
 
+build :: [LogMessage] -> MessageTree
+build logMessages = foldl (\tree logMessage -> Spring13.Week2.LogAnalysis.insert logMessage tree) Leaf logMessages
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder (Leaf) = []
+inOrder (Node Leaf logMessage Leaf) = [logMessage]
+inOrder (Node leftTree logMessage rightTree) = inOrder leftTree ++ [logMessage] ++ inOrder rightTree
+
 isLessThan :: LogMessage -> LogMessage -> Bool
 isLessThan (LogMessage _ newTimestamp _) (LogMessage _ oldTimestamp _) = newTimestamp < oldTimestamp
+
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong logMessages = getSevereErrors (inOrder (build logMessages))
+
+getSevereErrors :: [LogMessage] -> [String]
+getSevereErrors logMessages = map (getMessageFromLogMessage) (filter (filterSevereErros) logMessages)
+
+filterSevereErros :: LogMessage -> Bool
+filterSevereErros (LogMessage (Error severity) _ _) = severity >= 50
+filterSevereErros _ = False
+
+getMessageFromLogMessage :: LogMessage -> String
+getMessageFromLogMessage (LogMessage _ _ message) = message
+
+
+-- unused functions, dev purpose
+generateTree :: [String] -> MessageTree
+generateTree messages = foldl (\tree message -> Spring13.Week2.LogAnalysis.insert (parseMessage message) tree) Leaf messages
+
+generateSortedLogs :: [String] -> [LogMessage]
+generateSortedLogs messages = inOrder (foldl (\tree message -> Spring13.Week2.LogAnalysis.insert (parseMessage message) tree) Leaf messages)
