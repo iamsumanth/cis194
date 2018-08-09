99e3f0a7097230fac31c36cb1a46472ceba2d68c
Sumanth Kumar Mora <sumanthkumar537@gmail.com>
2018-08-09 14:16:23 +0530

    Extract data types

--- src/Spring13/Week12/Risk.hs
+++ src/Spring13/Week12/Risk.hs
@@ -1,45 +1,13 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE FlexibleContexts #-}
-
 module Spring13.Week12.Risk 
-  ( DieValue, Battlefield
+  ( DieValue
   ) where
 
-import Control.Monad.Random
 import Data.Sort
-
-------------------------------------------------------------
--- Die values
-
-newtype DieValue = DV { unDV :: Int } 
-  deriving (Eq, Ord, Show, Num)
-
-first :: (a -> b) -> (a, c) -> (b, c)
-first f (a, c) = (f a, c)
-
-instance Random DieValue where
-  random           = first DV . randomR (1,6)
-  randomR (low,hi) = first DV . randomR (max 1 (unDV low), min 6 (unDV hi))
-
-die :: Rand StdGen DieValue
-die = getRandom
+import Control.Monad.Random
+import Spring13.Week12.RiskTypes
 
 ------------------------------------------------------------
 -- Risk
-
--- threeInts :: Rand StdGen DieValue
--- threeInts = 
---   getRandom >>= (\x -> return [x])
-
-
-type Army = Int
-
-data Battlefield = Battlefield { attackers :: Army, defenders :: Army } deriving (Show)
-
-
-instance Monoid Battlefield where
-  mempty = Battlefield 0 0
-  mappend (Battlefield a1 d1) (Battlefield a2 d2) = Battlefield (a1 + a2) (d1 + d2)
 
 
 battle :: Battlefield -> Rand StdGen Battlefield
